46e8c138b511f21b9af898e93bf8dd02865ae3c1
ja-ho <magicuv@gmail.com>
2017-08-20 19:33:00 +0900

    update version

--- code/1261.cpp
+++ code/1261.cpp
@@ -4,14 +4,16 @@ using namespace std;
 
 int maze[111][111];
 int d[111][111];
+
 int dx[] = { 1, -1, 0, 0 };
 int dy[] = { 0, 0, 1, -1 };
 
 int bfs(int M, int N);
 
 int main() {
-	int M, N;
+	int N, M;
 	scanf("%d %d", &M, &N);
+
 	for (int i = 0; i < N; i++)
 	{
 		for (int j = 0; j < M; j++)
@@ -30,10 +32,11 @@ int main() {
 
 int bfs(int M, int N)
 {
-	queue<pair<int,int>> q;
+	queue<pair<int, int>> q;
 	queue<pair<int, int>> next_q;
-	q.push(make_pair(0,0));
 	d[0][0] = 0;
+	q.push(make_pair(0, 0));
+
 	while (!q.empty()) {
 		int currentX = q.front().first;
 		int currentY = q.front().second;
@@ -43,7 +46,7 @@ int bfs(int M, int N)
 			int nextX = currentX + dx[i];
 			int nextY = currentY + dy[i];
 
-			if (nextX >= 0 && nextY >= 0 && nextX < N && nextY < M) {
+			if (nextX >= 0 && nextX < N && nextY >= 0 && nextY < M) {
 				if (d[nextX][nextY] == -1) {
 					if (maze[nextX][nextY] == 0) {
 						d[nextX][nextY] = d[currentX][currentY];
@@ -56,11 +59,11 @@ int bfs(int M, int N)
 				}
 			}
 		}
+
 		if (q.empty()) {
 			q = next_q;
 			next_q = queue<pair<int, int>>();
 		}
 	}
-
-	return d[M - 1][N - 1];
+	return d[N - 1][M - 1];
 }
