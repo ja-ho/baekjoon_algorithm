7694323a14b35fd4cd512f2a5ecc873eac6a8ae9
ja-ho <magicuv@gmail.com>
2018-07-17 17:24:43 +0900

    some bruteforce

--- joon_assignment/2146(다리만들기).cpp
+++ joon_assignment/2146(다리만들기).cpp
@@ -1,29 +1,94 @@
 #include <iostream>
-#include <utility>
 #include <queue>
+#include <utility>
 using namespace std;
 
-int n;
+int N;
 int map[101][101];
-int visit[101][101];
+int group[101][101];
+int bridge[101][101];
 
-void bfs(int x, int y)
-{
-	
-}
+int dx[] = { 1, 0, -1, 0 };
+int dy[] = { 0, 1, 0, -1 };
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
+
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
 			cin >> map[i][j];
-			if (map[i][j] == 1) visit[i][j] = 0;
-			else visit[i][j] = -1;
 		}
 	}
 
+	//group ¸¸µé±â
+	int cnt = 0;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			if (map[i][j] == 1 && group[i][j] == 0) {
+				group[i][j] = ++cnt;
+				queue<pair<int, int>> q;
+				q.push(make_pair(j, i));
+				while (!q.empty()) {
+					int cx = q.front().first;
+					int cy = q.front().second;
+					q.pop();
 
+					for (int k = 0; k < 4; k++) {
+						int nx = cx + dx[k];
+						int ny = cy + dy[k];
+						if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+						if (map[ny][nx] == 1 && group[ny][nx] == 0) {
+							group[ny][nx] = cnt;
+							q.push(make_pair(nx, ny));
+						}
+					}
+				}
+			}
+		}
+	}
+	queue<pair<int, int>> q;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			bridge[i][j] = -1;
+			if (map[i][j] == 1) {
+				bridge[i][j] = 0;
+				q.push(make_pair(j, i));
+			}
+		}
+	}
+
+	while (!q.empty()) {
+		int cx = q.front().first;
+		int cy = q.front().second;
+		q.pop();
+		for (int k = 0; k < 4; k++) {
+			int nx = cx + dx[k];
+			int ny = cy + dy[k];
+			if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+			if (bridge[ny][nx] == -1) {
+				bridge[ny][nx] = bridge[cy][cx] + 1;
+				group[ny][nx] = group[cy][cx];
+				q.push(make_pair(nx, ny));
+			}
+		}
+	}
+
+	int answer = 1001;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			for (int k = 0; k < 4; k++) {
+				int x = j + dx[k];
+				int y = i + dy[k];
+				if (x < 0 || x >= N || y < 0 || y >= N) continue;
+				if (group[y][x] != group[i][j]) {
+					if (answer > bridge[i][j] + bridge[y][x]) answer = bridge[i][j] + bridge[y][x];
+				}
+			}
+		}
+	}
+
+	cout << answer << '\n';
 	return 0;
 }
